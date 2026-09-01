084698e0ff7d6f5da47a2cb27dbc1c996c955500
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2026-09-02 02:25:17 +0600

    feat: configure core better auth instance

---
+++ src/lib/auth.js
@@ -0,0 +1,39 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("sportNestDB");
+
+export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
+  baseURL: process.env.BETTER_AUTH_URL,
+
+  database: mongodbAdapter(db, {
+    client,
+  }),
+
+  emailAndPassword: {
+    enabled: true,
+  },
+    socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID, 
+            clientSecret: process.env.GOOGLE_SECRET, 
+        }, 
+    },
+    session:{
+      cookieCache: {
+        enabled: true,
+        strategy: "jwt",
+        // maxAge 7 days
+        maxAge: 7 * 24 * 60 * 60    
+      }
+    },
+    plugins: [
+      jwt()
+    ]
+});
