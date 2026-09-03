3e9d8a4f9343157a7df12fbb3b486f5feac5e553
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2026-09-03 21:31:26 +0600

    feat: implement My Bookings page for user reservation history

---
+++ src/app/my-bookings/page.jsx
@@ -0,0 +1,74 @@
+import BookingCard from "@/components/BookingCard";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import React from "react";
+import toast from "react-hot-toast";
+
+const MyBookingsPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  const {token} = await  auth.api.getToken({
+    headers: await headers()
+  })
+  const user = session?.user;
+
+  if (!user) {
+    redirect("/login");
+  }
+
+  let bookings = [];
+
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/booking/${user.id}`, {
+       headers:{
+       authorization : `Bearer ${token}`
+    },
+      cache: "no-store",
+    });
+
+    if (res.ok) {
+      bookings = await res.json();
+    }
+  } catch (error) {
+    bookings = [];
+  }
+
+  // console.log(bookings);
+
+  return (
+    <section className="min-h-screen bg-slate-50 px-4 py-12">
+      <div className="mx-auto max-w-7xl">
+        {bookings.length === 0 ? (
+          <div className="rounded-3xl border border-slate-200 bg-white p-10 text-center shadow-sm">
+            <h2 className="text-2xl font-bold text-slate-900">
+              No Booking Data Found
+            </h2>
+
+            <p className="mx-auto mt-3 max-w-md text-sm text-slate-600">
+              You have not booked any facility yet. Explore available sports
+              facilities and book your preferred time slot.
+            </p>
+
+            <Link
+              href="/facilities"
+              className="mt-6 inline-block rounded-xl bg-green-600 px-6 py-3 font-semibold text-white transition hover:bg-green-700"
+            >
+              Book a Facility
+            </Link>
+          </div>
+        ) : (
+          <div>
+            {bookings.map((booking) => (
+              <BookingCard key={booking._id} booking={booking} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default MyBookingsPage;
