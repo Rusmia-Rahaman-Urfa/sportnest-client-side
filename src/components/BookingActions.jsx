1e87336e4d860702c8dd59962085f1c8c49309ba
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2026-09-03 21:30:50 +0600

    feat: build BookingCard, action handlers, and cancel confirmation alert

---
+++ src/components/BookingActions.jsx
@@ -0,0 +1,146 @@
+"use client";
+
+import React, { useState } from "react";
+import { Button, DateField, Label } from "@heroui/react";
+import { Clock } from "lucide-react";
+import toast from "react-hot-toast";
+import { authClient } from "@/lib/auth-client";
+import { redirect } from "next/navigation";
+
+const BookingActions = ({ facility }) => {
+  const { data: session } = authClient.useSession();
+  const user = session?.user;
+
+  const {
+    name,
+    facility_type,
+    image,
+    location,
+    price_per_hour,
+    capacity,
+    available_slots,
+    description,
+    owner_email,
+    _id,
+  } = facility;
+
+  const [selectedSlot, setSelectedSlot] = useState("");
+  const [bookingDate, setBookingDate] = useState("");
+
+  const handleBookNow = async () => {
+    if (!user) {
+      toast.error("Please login first");
+      redirect("/login")
+      // return;
+    }
+
+    if (!bookingDate) {
+      toast.error("Please select a booking date");
+      return;
+    }
+
+    if (!selectedSlot) {
+      toast.error("Please select a time slot");
+      return;
+    }
+
+    const bookingData = {
+      userId: user.id,
+      userImage: user.image,
+      userName: user.name,
+      userEmail: user.email,
+      facilityId: _id,
+      name,
+      image,
+      facility_type,
+      location,
+      price_per_hour,
+      capacity,
+      description,
+      owner_email,
+      selectedSlot,
+      bookingDate: new Date(bookingDate),
+      hours: 1,
+      total_price: Number(price_per_hour),
+      status: "pending",
+    };
+
+    // console.log(bookingData);
+    const {data: tokenData} = await authClient.token();
+
+    // console.log(tokenData)
+
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/booking`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          authorization : `Bearer ${tokenData?.token}`
+        },
+        body: JSON.stringify(bookingData),
+      });
+
+      const data = await res.json();
+
+      if (data.insertedId) {
+        toast.success("Booking successful");
+      } else {
+        toast.error("Booking failed");
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <div className="mt-6 rounded-2xl bg-slate-50 p-4">
+      <DateField
+        className="w-full max-w-xs"
+        name="date"
+        onChange={(date) => setBookingDate(date)}
+      >
+        <Label className="mb-2 block text-sm font-semibold text-slate-700">
+          Booking Date
+        </Label>
+
+        <DateField.Group className="rounded-xl border border-slate-300 bg-white px-4 py-3 text-slate-900 outline-none focus-within:border-green-600">
+          <DateField.Input>
+            {(segment) => <DateField.Segment segment={segment} />}
+          </DateField.Input>
+        </DateField.Group>
+      </DateField>
+
+      <p className="mt-5 flex items-center gap-2 text-sm font-semibold text-slate-700">
+        <Clock size={18} className="text-green-600" />
+        Available Time Slots
+      </p>
+
+      <div className="mt-3 flex flex-wrap gap-2">
+        {available_slots?.map((slot, index) => (
+          <button
+            key={index}
+            type="button"
+            onClick={() => setSelectedSlot(slot)}
+            className={`rounded-full border px-4 py-2 text-sm font-medium shadow-sm transition  ${
+              selectedSlot === slot
+                ? "border-green-600 bg-green-600 text-white"
+                : "border-slate-200 bg-white text-slate-700 hover:border-green-600 hover:bg-green-50"
+            }`}
+          >
+            {slot}
+          </button>
+        ))}
+      </div>
+
+      <Button
+        type="button"
+        onPress={handleBookNow}
+        className="mt-5 rounded-xl bg-green-600 px-6 py-3 font-semibold text-white transition hover:bg-green-700"
+      >
+        Book Now
+      </Button>
+    </div>
+  );
+};
+
+export default BookingActions;
---
+++ src/components/BookingCancelAlert.jsx
@@ -0,0 +1,74 @@
+"use client"
+import { authClient } from "@/lib/auth-client";
+import { AlertDialog, Button } from "@heroui/react";
+import React from "react";
+import toast from "react-hot-toast";
+import { CiTrash } from "react-icons/ci";
+
+const BookingCancelAlert = ({ booking }) => {
+
+  const {
+    name,
+    _id
+  } = booking;
+//   console.log(_id)
+    // const {data: tokenData} = await authClient.token();
+  const handleDelete= async()=>{
+        const {data: tokenData} = await authClient.token();
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/booking/${_id}`,{
+        method: "DELETE",
+        headers:{
+            "content-type" : "application/json",
+              authorization : `Bearer ${tokenData?.token}`
+        }
+    })
+
+    const data = await res.json();
+    // console.log(data)
+
+    if(res.ok){
+        toast.success("Booking cancelled successfully")
+    window.location.reload();
+    }
+    if(!res.ok){
+        toast.error("Failed to cancel booking")
+    }
+  }
+  return (
+    <div>
+      <AlertDialog>
+        <Button className="text-red-500 bg-zinc-300 hover:bg-zinc-50 border-none" variant="outline">
+          <CiTrash /> Cancel
+        </Button>
+        <AlertDialog.Backdrop>
+          <AlertDialog.Container>
+            <AlertDialog.Dialog className="sm:max-w-100">
+              <AlertDialog.CloseTrigger />
+              <AlertDialog.Header>
+                <AlertDialog.Icon status="danger" />
+                <AlertDialog.Heading>Delete permanently?</AlertDialog.Heading>
+              </AlertDialog.Header>
+              <AlertDialog.Body>
+                <p>
+                  This will permanently delete <strong>{name}</strong> and all
+                  of its data. This action cannot be undone.
+                </p>
+              </AlertDialog.Body>
+              <AlertDialog.Footer>
+                <Button slot="close" variant="tertiary">
+                  Cancel
+                </Button>
+                <Button slot="close" onClick={handleDelete} variant="danger">
+                  Delete
+                </Button>
+              </AlertDialog.Footer>
+            </AlertDialog.Dialog>
+          </AlertDialog.Container>
+        </AlertDialog.Backdrop>
+      </AlertDialog>
+    </div>
+  );
+};
+
+export default BookingCancelAlert;
